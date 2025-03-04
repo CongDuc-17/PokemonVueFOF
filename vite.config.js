caf57c7626b02dec3e1ef6e7ef3b8435e44256d7
Cong Duc <tranlecongduc17@gmail.com>
2025-03-04 15:11:27 +0700

    2rd

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "PokemonVueFOF",
+  base: "/",
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
